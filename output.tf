cfcdbd4c5aa64eea89d1f90a931c2c1d9fa0cec4
Dinesh babu <dbramanayanam@gmail.com>
2024-04-10 21:39:30 +0400

    added few more files

--- output.tf
+++ output.tf
@@ -2,11 +2,16 @@ output "azs" {
   value = module.roboshop.azs
 }
 
-output "roboshop_vpc_id" {
-  value  = module.roboshop.vpc
+# output "roboshop_vpc_id" {
+#   value  = module.roboshop.vpc
 
-}
+# }
 
 #  output "database_subnets" {
 #     value       = module.roboshop.database_subnets
 #  }
+
+output "aws_route_table"{
+  value       = module.roboshop.aws_route_table
+
+}
