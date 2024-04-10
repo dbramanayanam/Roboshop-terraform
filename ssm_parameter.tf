e2c4c80f59347bcc4be24acc987b27fc069be6ef
Dinesh babu <dbramanayanam@gmail.com>
2024-04-10 17:26:51 +0400

    modified ssm parameters file

--- ssm_parameter.tf
+++ ssm_parameter.tf
@@ -8,5 +8,5 @@ resource "aws_ssm_parameter" "vpc" {
 resource "aws_ssm_parameter" "public_subnet_ids" {
   name  = "/${var.project}/${var.environment}/public_subnet_ids"
   type  = "StringList"
-  value = join(",", module.roboshop.public_subnet_ids.id)
+  value = join(",", module.roboshop.public_subnet_ids)
 }
