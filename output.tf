da815094ded62d3de144f2e0d4d7faa120c9e9bd
Dinesh babu <dbramanayanam@gmail.com>
2024-04-10 19:01:48 +0400

    added few more files

--- output.tf
+++ output.tf
@@ -6,18 +6,3 @@ output "azs" {
 output "vpc_id" {
   value = module.roboshop.vpc_id
 }
-
-
-output "public_subnet_ids" {
-  value = aws_subnet.public[*].id
-}
-
-
-output "private_subnet_ids" {
-  value = aws_subnet.private[*].id
-}
-
-
-output "database_subnet_ids" {
-  value = aws_subnet.database[*].id
-}
