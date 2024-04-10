a2dcd592aaa2b5f552c04fe5a3737e8deeeacfc1
Dinesh babu <dbramanayanam@gmail.com>
2024-04-10 17:37:04 +0400

    added few more files

--- ssm_parameter.tf
+++ ssm_parameter.tf
@@ -4,19 +4,7 @@ resource "aws_ssm_parameter" "vpc" {
   value = module.roboshop.vpc_id
 }
 
-output "public_subnet_ids" {
-  value = aws_subnet.public[*].id
-}
 
-
-output "private_subnet_ids" {
-  value = aws_subnet.private[*].id
-}
-
-
-output "database_subnet_ids" {
-  value = aws_subnet.database[*].id
-}
 # resource "aws_ssm_parameter" "public_subnet_ids" {
 #   name  = "/${var.project}/${var.environment}/public_subnet_ids"
 #   type  = "StringList"
