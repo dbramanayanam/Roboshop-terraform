db63bac4ccf8d7653e3cb7d38747c499a4ffabc3
Dinesh babu <dbramanayanam@gmail.com>
2024-04-10 20:49:04 +0400

    added few more files

--- output.tf
+++ output.tf
@@ -2,10 +2,10 @@ output "azs" {
   value = module.roboshop.azs
 }
 
-# output "roboshop_vpc_id" {
-#   value       = module.roboshop.vpc.id
+output "roboshop_vpc_id" {
+  value       = module.roboshop.roboshop_vpc_id.id
 
-# }
+}
 
 #  output "database_subnets" {
 #     value       = module.roboshop.database_subnets
