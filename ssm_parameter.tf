acbf773bbf715101848551a004f1a106e3c934b6
Dinesh babu <dbramanayanam@gmail.com>
2024-04-10 19:21:41 +0400

    added few more files

--- ssm_parameter.tf
+++ ssm_parameter.tf
@@ -5,20 +5,20 @@ resource "aws_ssm_parameter" "vpc" {
 }
 
 
-resource "aws_ssm_parameter" "public_subnet_ids" {
-    name = "/${var.project}/${var.environment}/public_subnet_ids"
-    type = "StringList"
-    value = join(",", module.roboshop.public_subnet_ids)
-}
+# resource "aws_ssm_parameter" "public_subnet_ids" {
+#     name = "/${var.project}/${var.environment}/public_subnet_ids"
+#     type = "StringList"
+#     value = join(",", module.roboshop.public_subnet_ids)
+# }
 
-resource "aws_ssm_parameter" "private_subnet_ids" {
-    name = "/${var.project}/${var.environment}/private_subnet_ids"
-    type = "StringList"
-    value = join(",", module.roboshop.private_subnet_ids)
-}
+# resource "aws_ssm_parameter" "private_subnet_ids" {
+#     name = "/${var.project}/${var.environment}/private_subnet_ids"
+#     type = "StringList"
+#     value = join(",", module.roboshop.private_subnet_ids)
+# }
 
-resource "aws_ssm_parameter" "database_subnet_ids" {
-    name = "/${var.project}/${var.environment}/database_subnet_ids"
-    type = "StringList"
-    value = join(",", module.roboshop.database_subnet_ids)
-}
+# resource "aws_ssm_parameter" "database_subnet_ids" {
+#     name = "/${var.project}/${var.environment}/database_subnet_ids"
+#     type = "StringList"
+#     value = join(",", module.roboshop.database_subnet_ids)
+# }
